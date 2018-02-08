1f1a0dd23fd2848fcecc9320145754635769fa6f
Marco Roa <roa.marco@gmail.com>
2018-02-07 20:46:35 -0600

    Changed demo
    Console prints Hello World

--- src/functions/demo.ts
+++ src/functions/demo.ts
@@ -1,10 +1,6 @@
-'use strict';
-import { success, failure, notAllowed } from './../libs/response-lib';
+"use strict";
+import { success, failure, notAllowed } from "./../libs/response-lib";
 
 export async function demo(event, context, callback) {
-  console.log(
-    'Tocayolog:' + JSON.stringify(event),
-    JSON.stringify(context),
-    JSON.stringify(callback)
-  );
+  console.log("Hello World");
 }
